a69dbab050952cef9fdc5b07a67da535c82a413a
Mazen Eldaly <dalydevelopment15@gmail.com>
2025-01-13 11:21:52 +0200

    Add SVG assets for Facebook, Apple, and Google; update AndroidManifest and app_colors

---
+++ lib/core/utils/app_images.dart
@@ -0,0 +1,20 @@
+// ignore_for_file: prefer_single_quotes
+class Assets {
+  Assets._();
+
+  /// Assets for imagesApple
+  /// assets/images/apple.svg
+  static const String imagesApple = "assets/images/apple.svg";
+
+  /// Assets for imagesFacebook
+  /// assets/images/facebook.svg
+  static const String imagesFacebook = "assets/images/facebook.svg";
+
+  /// Assets for imagesGoogle
+  /// assets/images/google.svg
+  static const String imagesGoogle = "assets/images/google.svg";
+
+  /// Assets for imagesVectorMan
+  /// assets/images/vector_man.svg
+  static const String imagesVectorMan = "assets/images/vector_man.svg";
+}
