f7738b653114cacbde74054b75bda92bcf6ce119
Mazen Eldaly <dalydevelopment15@gmail.com>
2025-01-13 11:30:14 +0200

    Integrate Device Preview and refactor main app structure; add StartAppView and StartAppBody widgets

---
+++ lib/featuers/start/presentation/views/start_app_view.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:login_ui/featuers/start/presentation/views/widgets/start_app_body.dart';
+
+class StartAppView extends StatelessWidget {
+  const StartAppView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: StartAppBody(),
+    );
+  }
+}
---
+++ lib/featuers/start/presentation/views/widgets/start_app_body.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class StartAppBody extends StatelessWidget {
+  const StartAppBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Column();
+  }
+}
